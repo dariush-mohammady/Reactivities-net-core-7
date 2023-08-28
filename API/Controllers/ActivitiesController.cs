cebabe08827ea2ae4025d72361b5034e3c3a9fe4
darius <darius134966@gmail.com>
2023-08-28 11:26:29 +0330

    add Identity-basics

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,6 +1,7 @@
 using Application.Activities;
 using Microsoft.AspNetCore.Mvc;
 using Domain;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class UserDto
+    {
+        public string DisplayName { get; set; }
+
+        public string Token { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Image { get; set; }
+    }
+}
